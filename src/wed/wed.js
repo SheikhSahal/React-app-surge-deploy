0f39ee34b8c50635e3298168a703c5409b6d29e8
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2020-09-17 18:01:23 +0500

    Update wed.js

--- src/wed/wed.js
+++ src/wed/wed.js
@@ -3,7 +3,7 @@ import React from 'react'
 function wed(props) {
     return (
         <div>
-            {props.behavior} Wed Name: <b>{props.names}</b>
+            {props.behavior} Weds Name: <b>{props.names}</b>
         </div>
     )
 }
